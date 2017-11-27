fde429f5acebf4835d0bee85230406fd55b2152f
Ryan Blunden <ryan.blunden@gmail.com>
2017-11-27 13:57:27 +1100

    Adjusted the docker compose files to move development settings into `docker-compose-dev.yml` and made entrypoint.sh more production ready.

--- catswhocode/catswhocode/bin/entrypoint.sh
+++ catswhocode/catswhocode/bin/entrypoint.sh
@@ -1,22 +1,77 @@
 #!/bin/bash
 
+#--------------------------------------------
+# ENTRYPOINT SCRIPT
+#
+#  - Database is assumed to be created.
+#  - Django admin user is not created as part of this.
+#  - No special behaviour to be added for the initial run. That is outside this scripts responsibilities.
+#  - Set `DJANGO_DEBUG` to trigger installation of Python dev dependencies.
+#  - Set `DJANGO_MANAGEPY_MIGRATE` to trigger migrations.
+#  - Set `DJANGO_MANAGEPY_COLLECTSTATIC` to trigger the collection of static files.
+#
+#--------------------------------------------
+
+# TODO: Move this into the wsgi.py file so application code isn't maintained in multiple places.
+
 set -e
 
-./bin/wait-for-it.sh db:3306 -- echo '[info]: MySQL is up.'
 
-./bin/wait-for-it.sh cache:6379 -- echo '[info]: Redis is up.'
+#--------------------------------------------
+# Check required environment vars are set
+#--------------------------------------------
 
-if [ "x$DJANGO_DEBUG" = 'xon' ]; then
-    echo '[info]: Installing development packages as DJANGO_DEBUG is set.'
-    pipenv install --dev
+required_vars=(DJANGO_SECRET_KEY DB_NAME DB_USER DB_PASSWORD DB_HOST DB_PORT CACHE_HOST CACHE_PORT)
+
+missing_vars=()
+for i in "${required_vars[@]}"
+do
+    test -n "${!i+set}" || missing_vars+=("$i")
+done
+
+if [ ${#missing_vars[@]} -gt 0 ]
+then
+    echo "[error]: Aborting application start as required environment vars are not set:" >&2
+    printf ' - %q\n' "${missing_vars[@]}" >&2
+    exit 1
 fi
 
-if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
+
+#-----------------------------------------------------------
+# Wait for MySQL and Redis to be accepting connections
+#-----------------------------------------------------------
+
+./bin/wait-for-it.sh db:3306 -- echo '[info]: MySQL is up.'
+./bin/wait-for-it.sh cache:6379 -- echo '[info]: Redis is up.'
+
+
+#-------------------------------------------------------------------------------
+# Optional vars for specifying additional operations before application start
+#-------------------------------------------------------------------------------
+
+if [ -v DJANGO_DEBUG ]; then
+    echo '[info]: Installing development packages as DJANGO_DEBUG is set.'
+    pipenv install --dev --deploy --system
+fi
+
+if [ -v DJANGO_MANAGEPY_MIGRATE ]; then
+    echo '[info]: Performing database migrations as DJANGO_MANAGEPY_MIGRATE is set.'
     ./manage.py migrate --noinput
 fi
 
-if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
+if [ -v DJANGO_MANAGEPY_COLLECTSTATIC ]; then
+    echo '[info]: Collecting static resources as DJANGO_MANAGEPY_COLLECTSTATIC is set.'
     ./manage.py collectstatic --noinput
 fi
 
+if [ -z DJANGO_DEBUG ]; then
+    ./manage.py check --deploy
+fi
+
+
+#-------------
+# Start!
+#-------------
+
+echo "[info]: Starting application with command - \"${@}.\""
 exec "$@"
